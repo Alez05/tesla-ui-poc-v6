4e138054a1db748aa8068f231876af8d7b551961
alez05 <cercel.alexandru436@gmail.com>
2023-01-05 12:06:41 +0000

    another rainy day

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,6 +3,7 @@ const TeslaUIPOCV6 = () => {
     <div>
       <h1>Tesla UI V6 - Proof Of Concept</h1>
       <p>Let's get down to conding!</p>
+      <p>This is Alex's project</p>
     </div>
   )
 }
